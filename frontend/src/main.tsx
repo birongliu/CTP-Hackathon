d055402cd971c247b1ff469b809476e130a31721
birongliu <birongliu2003@gmail.com>
2025-08-22 10:19:29 -0400

    Refactor Navbar to conditionally render based on interview path; add InterviewSummary route and styles; enhance HomePage with interview starting functionality and UI updates; adjust SummaryPage for session ID handling; fix SignInOutForm CSS padding.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/index.css'
-import App from './routes/App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import SignPage from './routes/SignPage.tsx'
-import TechnicalPage from './routes/TechnicalPage.tsx';
-import InterviewRoom from './routes/InterviewRoom.tsx'
-import Behavioral from './routes/Behavioral.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/index.css";
+import App from "./routes/App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignPage from "./routes/SignPage.tsx";
+import TechnicalPage from "./routes/TechnicalPage.tsx";
+import InterviewRoom from "./routes/InterviewRoom.tsx";
+import InterviewSummary from "./routes/InterviewSummary.tsx";
+import Behavioral from "./routes/Behavioral.tsx";
 
 const router = createBrowserRouter([
-  {path: '/', element: <App />},
-  {path: '/signin', element: <SignPage />},
-  {path: '/signup', element: <SignPage />},
-  {path: '/technical', element: <TechnicalPage />},
-  {path: '/behavioral', element: <Behavioral/>},
-  {path: '/signup', element: <SignPage />},
-  {path: '/interview', element: <InterviewRoom />},
-  {path: '/interview/:sessionId', element: <InterviewRoom />}
-])
+  { path: "/", element: <App /> },
+  { path: "/signin", element: <SignPage /> },
+  { path: "/signup", element: <SignPage /> },
+  { path: "/technical", element: <TechnicalPage /> },
+  { path: "/behavioral", element: <Behavioral /> },
+  { path: "/interview", element: <InterviewRoom /> },
+  { path: "/interview/:sessionId", element: <InterviewRoom /> },
+  { path: "/summary/:sessionId", element: <InterviewSummary /> },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
